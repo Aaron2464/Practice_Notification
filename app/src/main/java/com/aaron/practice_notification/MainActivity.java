84d04b3cdd6cbd5290cbf3f4de53be04b01f3f0c
tim2464 <tim2464@yahoo.com.tw>
2018-07-23 21:36:38 +0800

    add small icon & let notification work

--- app/src/main/java/com/aaron/practice_notification/MainActivity.java
+++ app/src/main/java/com/aaron/practice_notification/MainActivity.java
@@ -1,13 +1,73 @@
 package com.aaron.practice_notification;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButton = findViewById(R.id.send);
+        mButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.send:
+                senNotification();
+                break;
+        }
+    }
+
+    private void senNotification() {
+        String title = "Notification";
+        String content = "Succeed";
+        CharSequence name = "渠道名稱1";
+        String description = "渠道描述1";
+        String channelId = "channelId1";        //渠道id
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        Notification.Builder builder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //創建通知渠道
+            int importance = NotificationManager.IMPORTANCE_HIGH;       //重要性級別
+            NotificationChannel mChannel = new NotificationChannel(channelId, name, importance);
+            mChannel.setDescription(description);   //渠道描述
+            mChannel.enableLights(true);            //是否顯示通知指示燈
+            mChannel.enableVibration(true);         //是否振動
+            builder = new Notification.Builder(this, channelId)
+                    .setSmallIcon(R.drawable.run)           // small icon must be png, and background must be transparent.
+                    .setContentTitle(title)
+                    .setContentText(content)
+                    .setChannelId(channelId);
+            notificationManager.createNotificationChannel(mChannel);//創建通知渠道
+            notificationManager.notify(new Random().nextInt(), builder.build());
+        } else {
+            builder = new Notification.Builder(this)
+                    .setSmallIcon(R.drawable.run)
+                    .setContentTitle(title)
+                    .setContentText(content);
+            Notification notification = builder.build();
+            notification.flags |= Notification.FLAG_AUTO_CANCEL;
+            notification.defaults |= Notification.DEFAULT_SOUND;
+            notificationManager.notify(new Random().nextInt(), notification);  // if id = id , notification just has only one;
+        }
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+        builder.setContentIntent(contentIntent);
     }
 }
